a8cb43e7da12f6b1b9d457b740d7dbbd93e3e6e7
Tuan Nguyen <tuan.nguyen@elsanow.io>
2025-03-06 23:21:08 +0700

    chore: remove redundant instances

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,28 +11,8 @@ module.exports = {
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
     {
-      name: "cabquiz.room.evaluate.worker_2",
-      script: "node room.evaluate.js worker_2",
-      cwd: "/home/ubuntu/CabQuiz/backend",
-    },
-    {
-      name: "cabquiz.room.evaluate.worker_3",
-      script: "node room.evaluate.js worker_3",
-      cwd: "/home/ubuntu/CabQuiz/backend",
-    },
-    {
       name: "cabquiz.question.context.generate.worker_1",
       script: "node question.context.generate.js worker_1",
-      cwd: "/home/ubuntu/CabQuiz/backend",
-    },
-    {
-      name: "cabquiz.question.context.generate.worker_2",
-      script: "node question.context.generate.js worker_2",
-      cwd: "/home/ubuntu/CabQuiz/backend",
-    },
-    {
-      name: "cabquiz.question.context.generate.worker_3",
-      script: "node question.context.generate.js worker_3",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
   ],
