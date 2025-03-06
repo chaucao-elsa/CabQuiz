7a3cf676d4024bb1a1319d7f088da3735a2b3219
Tuan Nguyen <tuan.nguyen@elsanow.io>
2025-03-06 21:30:11 +0700

    fix: script name

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: "cabquiz.question.free.worker_1",
+      name: "cabquiz.question.free",
       script: "question.free.js",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
     {
       name: "cabquiz.room.evaluate.worker_1",
-      script: "room.evaluate.js worker_1",
+      script: "node room.evaluate.js worker_1",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
     {
       name: "cabquiz.room.evaluate.worker_2",
-      script: "room.evaluate.js worker_2",
+      script: "node room.evaluate.js worker_2",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
     {
       name: "cabquiz.room.evaluate.worker_3",
-      script: "room.evaluate.js worker_3",
+      script: "node room.evaluate.js worker_3",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
   ],
