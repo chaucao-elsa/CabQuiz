ceb292961254880f7257ce8e0fc7d7701c258616
Tuan Nguyen <tuan.nguyen@elsanow.io>
2025-03-06 23:23:07 +0700

    chore: add instances

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,8 +11,18 @@ module.exports = {
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
     {
+      name: "cabquiz.room.evaluate.worker_2",
+      script: "node room.evaluate.js worker_2",
+      cwd: "/home/ubuntu/CabQuiz/backend",
+    },
+    {
       name: "cabquiz.question.context.generate.worker_1",
       script: "node question.context.generate.js worker_1",
+      cwd: "/home/ubuntu/CabQuiz/backend",
+    },
+    {
+      name: "cabquiz.question.context.generate.worker_2",
+      script: "node question.context.generate.js worker_2",
       cwd: "/home/ubuntu/CabQuiz/backend",
     },
   ],
